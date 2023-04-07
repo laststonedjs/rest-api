fb6fa1f3235ffb468c1c6705460694fb86c05431
Filip <filipsarovic428@gmail.com>
2023-04-07 10:06:31 +0200

    Implemented logic to getting and query data

--- controllers/players.js
+++ controllers/players.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 import Player from "../models/Player.js"
 
 export const getPlayers = async (req, res) => {
@@ -11,4 +11,14 @@ export const getPlayers = async (req, res) => {
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
+}
+
+export const getPlayerById = async (req, res) => {
+  const { id: _id } = req.params;
+  const player = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No player with that id');
+  const updatedPlayer = await Player.findByIdAndUpdate(_id, { ...player, _id }, { new: true });
+
+  res.json(updatedPlayer);
 }
